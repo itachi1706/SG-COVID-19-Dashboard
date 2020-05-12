183a593cf18b202d98ec6a52ac1121108f13b59f
itachi1706 <kennethsohyq@gmail.com>
2020-05-12 19:19:06 +0800

    Removed usage of firebaseUI as it doesnt match
    We used our own custom one instead
    
    Took 23 minutes

--- routes/firebase.js
+++ routes/firebase.js
@@ -1,5 +1,5 @@
 // These contain unique, non-secret keys and can be committed
-const firebaseConfig = {
+module.exports.firebaseConfig = {
     apiKey: "AIzaSyCkKkEWlAdfVfBoqHaK7lYqmmG9wdzbvmQ",
     authDomain: "ccn-webapps.firebaseapp.com",
     databaseURL: "https://ccn-webapps.firebaseio.com",
@@ -9,29 +9,3 @@ const firebaseConfig = {
     appId: "1:985951588863:web:ffc796cdd26ec65cb657ca",
     measurementId: "G-WE3YTSB1MJ"
 };
-
-const firebase = require('firebase');
-const firebaseui = require('firebaseui');
-
-let ui = new firebaseui.auth.AuthUI(firebase.auth);
-
-module.exports.signin = function() {
-    let uiConfig = {
-        callbacks: {
-            signInSuccessWithAuthResult(authResult, redirectUrl) {
-                // Successful sign in
-                return true;
-            },
-            uiShown() {
-                // Widget rendered, hide loader
-                document.getElementById('loader').style.display = 'none';
-            }
-        },
-        signInFlow: 'popup',
-        signInSuccessUrl: 'redirecturl',
-        signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
-        tosUrl: '',
-        privacyPolicyUrl: ''
-    };
-    ui.start('#firebaseui-auth-container', uiConfig);
-}
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-//var firebaseutil = require('./firebase');
+var firebaseutil = require('./firebase');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,21 +11,19 @@ router.get('/', function(req, res, next) {
 router.get('/navbartest', function (req, res) {
   let loggedin = (req.query.log === '1');
   console.log(loggedin);
-  res.render('test-navbar', { username: 'Test', loggedIn: loggedin, route: 'cd' });
+  res.render('test-navbar', { username: 'Test', loggedIn: loggedin, route: 'cd', fbconfig: '' });
 });
 
-router.get('/login', function (req, res) {
-  res.redirect('/navbartest?log=1');
-});
+// router.get('/login', function (req, res) {
+//   res.redirect('/navbartest?log=1');
+// });
 
 router.get('/logout', function (req, res) {
   res.redirect('/navbartest');
 });
 
-router.get('/signin', function (req, res) {
-  // TODO: Will replace /login when completed
-  //firebaseutil.signin();
-  res.render('login', { route: '' })
+router.get('/login', function (req, res) {
+  res.render('login', { route: '', fbconfig: JSON.stringify(firebaseutil.firebaseConfig) })
 })
 
 module.exports = router;
