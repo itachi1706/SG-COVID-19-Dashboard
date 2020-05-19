6177f33d11cb87dfa31beb7ed16388b9e3535f7f
itachi1706 <kennethsohyq@gmail.com>
2020-05-19 20:21:06 +0800

    Finish up on the form filling section of adding statistics
    We have also properly updated and coded in the calculation of statistics as each data is inserted into the page and the calculation according to tests are correct as well
    
    Took 34 minutes

--- routes/admin.js
+++ routes/admin.js
@@ -32,14 +32,19 @@ router.get('/', function (req, res) {
 router.get('/add', async function (req, res) {
     console.log(`SELECT * FROM ${dbConfig.infoTable}`);
     let prevDayResults = [];
+    let newDay = -1;
+    let data = {}
 
     try {
-        let result = await db.query(`SELECT * FROM ${dbConfig.infoTable} LIMIT 1`);
-        let data = result[0]; // Only 1 record expected, the previous day records
+        let result = await db.query(`SELECT * FROM ${dbConfig.infoTable} ORDER BY Day DESC LIMIT 1`);
+        data = result[0]; // Only 1 record expected, the previous day records
         for (let d in data) {
             let t = {name: infoModel[d], val: data[d]};
             prevDayResults.push(t);
         }
+        // Some autofill stats
+        newDay = data.Day + 1;
+        console.log("New Day: " + newDay);
     } catch (e) {
         console.log(e);
     }
@@ -47,9 +52,10 @@ router.get('/add', async function (req, res) {
     // Set a default timestamp of today 1200h. Must format to something like "2019-01-01T11:11"
     let defaultDate = new Date();
     defaultDate.setHours(12, 0, 0);
+    console.log(data);
 
     res.render('addstats', { route: 'admin', fbConfig: frontend.getFirebaseConfig(), username: res.locals.name, loggedIn: res.locals.authed,
-       prevData: prevDayResults, model: infoModel, defaultDate: utilFunc.toISOLocal(defaultDate).substr(0, 16) });
+       prevData: prevDayResults, model: infoModel, defaultDate: utilFunc.toISOLocal(defaultDate).substr(0, 16), newDay: newDay, prevDataRaw: JSON.stringify(data) });
 });
 
 module.exports = router;
