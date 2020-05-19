46c2e54a4fa92c324f0e7a30c826e6e78c1a7601
itachi1706 <kennethsohyq@gmail.com>
2020-05-19 17:21:25 +0800

    Nicer formatting for previous day statistics
    Took 12 minutes

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,8 @@ const frontend = require('../api/firebase-frontend');
 const {dbConfig} = require('../config');
 const db = require('../api/db');
 
+const infoModel = require('../model/info');
+
 // Check Authentication. Unauthenticated users get kicked to login page
 async function checkAuth(req, res, next) {
     let authCheck = await auth.checkAuth(req, res, next);
@@ -28,22 +30,21 @@ router.get('/', function (req, res) {
 
 router.get('/add', async function (req, res) {
     console.log(`SELECT * FROM ${dbConfig.infoTable}`);
-    let cols = [];
-    let rows = [];
+    let prevDayResults = [];
 
     try {
         let result = await db.query(`SELECT * FROM ${dbConfig.infoTable} LIMIT 1`);
         let data = result[0]; // Only 1 record expected, the previous day records
         for (let d in data) {
-            cols.push(d);
-            rows.push(data[d]);
+            let t = {name: infoModel[d], val: data[d]};
+            prevDayResults.push(t);
         }
     } catch (e) {
         console.log(e);
     }
 
     res.render('addstats', { route: 'admin', fbConfig: frontend.getFirebaseConfig(), username: res.locals.name, loggedIn: res.locals.authed,
-       cols: cols, rows: rows});
+       prevData: prevDayResults});
 });
 
 module.exports = router;
