b032665b6585b561ab1d4c0b27a0ebefc392a436
itachi1706 <kennethsohyq@gmail.com>
2020-05-21 18:47:03 +0800

    Various changes to implement loading of case history
    - Added DataTables and Font Awesome into header
    - Switched jQuery to full jQuery instead of slim to make use of $.ajax()
    - Added a datatable of case history itself with pagination support
    - Added another route to obtain the data required through ajax call
    - Switched options to contain a default object like what was done for the administration panel
    
    Took 57 minutes

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,16 @@ const router = express.Router();
 
 const frontend = require('../api/firebase-frontend');
 const auth = require('../api/firebase-auth');
+const db = require('../api/db');
+
+const defaultObj = { fbConfig: frontend.getFirebaseConfig(), username: "Unknown", loggedIn: false };
 
 async function checkAuth(req, res, next) {
-  await auth.checkAuth(req, res, next);
+  let token = await auth.checkAuth(req, res, next);
+  if (token) {
+    defaultObj.username = res.locals.name;
+    defaultObj.loggedIn = res.locals.authed;
+  }
   next();
 }
 
@@ -17,14 +24,28 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/navbartest', function (req, res) {
-  res.render('test-navbar', { route: 'cd', fbConfig: frontend.getFirebaseConfig(), username: res.locals.name, loggedIn: res.locals.authed });
+  res.render('test-navbar', { ...defaultObj, route: 'cd' });
 });
 
 router.get('/login', function (req, res) {
   let error = '';
   if (req.query.err) error = auth.failErrors[req.query.err];
   if (error === auth.failErrors[1]) res.status(401); // Set 401 Unauthorized if coming from unauth users
-  res.render('login', { route: '', fbConfig: frontend.getFirebaseConfig(), errorCode: error, username: res.locals.name, loggedIn: res.locals.authed })
+  res.render('login', { ...defaultObj, route: '', errorCode: error })
+});
+
+router.get('/casehistory', async function (req, res) {
+  res.render('viewcase', {...defaultObj, route: 'cd'});
+});
+
+router.get('/casehistory/data', async function(req, res) {
+  let output = {data: []};
+  try {
+    output.data = await db.query('SELECT * FROM sg_case_info');
+  } catch (e) {
+    console.log(e);
+  }
+  res.json(output);
 });
 
 module.exports = router;
