07b287cfad905a4fe770967d3d34743a64b4a586
itachi1706 <kennethsohyq@gmail.com>
2020-05-21 21:31:10 +0800

    Added compression to shrink size of ajax calls for json data
    We also moved jQuery down to the body as we do not exactly need it to load before the navbar loads
    
    Took 12 minutes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
+const compression = require('compression');
 
 const indexRouter = require('./routes/index');
 const adminRouter = require('./routes/admin');
@@ -18,6 +19,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
