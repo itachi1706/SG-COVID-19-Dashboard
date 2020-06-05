3a0803ce6ad7fffef2801832178a6e0e6ee3673c
Kenneth Soh <kennethsohyq@gmail.com>
2020-06-05 14:45:10 +0000

    Add Google API's Secure Token for Firebase Token refreshing

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(helmet.contentSecurityPolicy({
     styleSrc: ["'self'", "'unsafe-inline'", 'fonts.googleapis.com', 'unpkg.com', 'cdn.datatables.net', 'www.gstatic.com'],
     scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'kit.fontawesome.com', 'kit-pro.fontawesome.com', 'code.jquery.com', 'www.gstatic.com', 'unpkg.com', 'cdn.datatables.net', 'cdnjs.cloudflare.com', 'www.google-analytics.com'],
     fontSrc: ["'self'", 'fonts.gstatic.com'],
-    connectSrc: ["'self'", 'www.googleapis.com', 'kit-pro.fontawesome.com']
+    connectSrc: ["'self'", 'www.googleapis.com', 'kit-pro.fontawesome.com', 'securetoken.googleapis.com']
   }
 }));
 app.use(helmet.referrerPolicy({policy: 'same-origin'}));
