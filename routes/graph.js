fb6d677da1c34d4533b7fa3a803f8890258a4496
itachi1706 <kennethsohyq@gmail.com>
2020-07-08 16:04:37 +0800

    Implemented Confirmed/Discharged Cases graph
    Closes #58
    
    Took 16 minutes

--- routes/graph.js
+++ routes/graph.js
@@ -37,6 +37,12 @@ router.get('/discharged', async function (req, res) {
     datasource: 'discharged', type: "Bar", co: JSON.stringify(chartOptions)});
 });
 
+router.get('/confirmeddischarged', async function (req, res) {
+  let chartOptions = {chart: {title: 'Confirmed and Discharged Cases', subtitle: 'Daily confirmed and discharged cases in SG'}, series: {0: {color: "#FF0000"}, 1: {color: "#00FF00"}}};
+  res.render('googlegraph', {...defaultGraphObj, title: 'Confirmed & Discharged Cases Chart - COVID-19 Dashboard (SG)', gt: 'Confirmed & Discharged Cases',
+    datasource: 'confirmeddischarged', type: "Line", co: JSON.stringify(chartOptions)});
+});
+
 router.get('/data/cumulative', async function (req, res) {
   try {
     let output = await db.query(`SELECT Day, Date, CumulativeConfirmed, CumulativeDischarged FROM ${dbConfig.infoTable}`);
@@ -84,6 +90,25 @@ router.get('/data/discharged', async function (req, res) {
     output.forEach((d) => {
       let date = new Date(d.Date);
       rows.push({c:[{v:date.toDateString()}, {v: parseInt(d.Recovered_Day) + parseInt(d.Deaths_Day)}]});
+    });
+    gDataShell.rows = rows;
+    res.json(gDataShell);
+  } catch (e) {
+    res.status(404);
+    res.json({error: e});
+    res.end();
+  }
+});
+
+router.get('/data/confirmeddischarged', async function (req, res) {
+  try {
+    let output = await db.query(`SELECT Day, Date, ConfirmedCases_Day, Recovered_Day, Deaths_Day FROM ${dbConfig.infoTable}`);
+    let gDataShell = {};
+    gDataShell.cols = [{label: "Time", type: "string"}, {id: "cnf", label: "Confirmed Cases", type: "number"}, { id: "disd", label: "Discharged Cases", type: "number" }];
+    let rows = [];
+    output.forEach((d) => {
+      let date = new Date(d.Date);
+      rows.push({c:[{v:date.toDateString()}, {v: parseInt(d.ConfirmedCases_Day)}, {v: parseInt(d.Recovered_Day) + parseInt(d.Deaths_Day)}]});
     });
     gDataShell.rows = rows;
     res.json(gDataShell);
