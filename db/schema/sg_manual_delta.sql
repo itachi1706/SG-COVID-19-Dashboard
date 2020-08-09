c1ec97ae247407a04da70904a559a71921ce047d
Kenneth Soh <kennethsohyq@gmail.com>
2020-08-09 18:20:26 +0800

    Make the database structures neater and formatted
    This makes it easier to view
    
    Took 32 seconds

--- db/schema/sg_manual_delta.sql
+++ db/schema/sg_manual_delta.sql
@@ -11,9 +11,9 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
@@ -26,38 +26,41 @@ SET time_zone = "+00:00";
 -- Table structure for table `sg_manual_delta`
 --
 
-CREATE TABLE `sg_manual_delta` (
-  `id` int(11) NOT NULL,
-  `Day` int(11) NOT NULL COMMENT 'Foreign Key referencing details table',
-  `ConfirmedCases_Day` int(11) NOT NULL DEFAULT '-1',
-  `ImportedCase_Day` int(11) NOT NULL DEFAULT '-1',
-  `TotalLocalCase_Day` int(11) NOT NULL DEFAULT '0',
-  `LocalLinked` int(11) NOT NULL DEFAULT '0',
-  `LocalUnlinked` int(11) NOT NULL DEFAULT '-1',
-  `Hospital_OtherAreas` int(11) NOT NULL DEFAULT '0',
-  `HospitalizedTotal` int(11) NOT NULL DEFAULT '0',
-  `HospitalizedStable` int(11) NOT NULL DEFAULT '0',
-  `HospitalizedICU` int(11) NOT NULL DEFAULT '-1',
-  `HospitalizedOtherArea` int(11) NOT NULL DEFAULT '-1',
-  `Recovered_Day` int(11) NOT NULL DEFAULT '-1' COMMENT '(INC-GREEN)',
-  `Deaths_Day` int(11) NOT NULL DEFAULT '-1',
-  `CumulativeConfirmed` int(11) NOT NULL DEFAULT '0',
-  `CumulativeImported` int(11) NOT NULL DEFAULT '0',
-  `CumulativeLocal` int(11) NOT NULL DEFAULT '0',
-  `CumulativeRecovered` int(11) NOT NULL DEFAULT '0' COMMENT '(INC-GREEN)',
-  `CumulativeDeaths` int(11) NOT NULL DEFAULT '0',
-  `CumulativeDischarged` int(11) NOT NULL DEFAULT '0' COMMENT 'Deaths + Recovered (INC-GREEN)',
-  `DailyQuarantineOrdersIssued` int(11) NOT NULL DEFAULT '0',
-  `TotalCloseContacts` int(11) NOT NULL DEFAULT '-1',
-  `Quarantined` int(11) NOT NULL DEFAULT '0',
-  `CompletedQuarantine` int(11) NOT NULL DEFAULT '0' COMMENT '(INC-GREEN)',
-  `QUO_Pending` int(11) NOT NULL DEFAULT '0',
-  `QUO_TransferHospital` int(11) NOT NULL DEFAULT '0',
-  `QUO_NonGazettedDorm` int(11) NOT NULL DEFAULT '0',
-  `QUO_GazettedDorm` int(11) NOT NULL DEFAULT '0',
-  `QUO_GovtQuarantinedFacilities` int(11) NOT NULL DEFAULT '0',
-  `QUO_HomeQuarantinedOrder` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE `sg_manual_delta`
+(
+    `id`                            int(11) NOT NULL,
+    `Day`                           int(11) NOT NULL COMMENT 'Foreign Key referencing details table',
+    `ConfirmedCases_Day`            int(11) NOT NULL DEFAULT '-1',
+    `ImportedCase_Day`              int(11) NOT NULL DEFAULT '-1',
+    `TotalLocalCase_Day`            int(11) NOT NULL DEFAULT '0',
+    `LocalLinked`                   int(11) NOT NULL DEFAULT '0',
+    `LocalUnlinked`                 int(11) NOT NULL DEFAULT '-1',
+    `Hospital_OtherAreas`           int(11) NOT NULL DEFAULT '0',
+    `HospitalizedTotal`             int(11) NOT NULL DEFAULT '0',
+    `HospitalizedStable`            int(11) NOT NULL DEFAULT '0',
+    `HospitalizedICU`               int(11) NOT NULL DEFAULT '-1',
+    `HospitalizedOtherArea`         int(11) NOT NULL DEFAULT '-1',
+    `Recovered_Day`                 int(11) NOT NULL DEFAULT '-1' COMMENT '(INC-GREEN)',
+    `Deaths_Day`                    int(11) NOT NULL DEFAULT '-1',
+    `CumulativeConfirmed`           int(11) NOT NULL DEFAULT '0',
+    `CumulativeImported`            int(11) NOT NULL DEFAULT '0',
+    `CumulativeLocal`               int(11) NOT NULL DEFAULT '0',
+    `CumulativeRecovered`           int(11) NOT NULL DEFAULT '0' COMMENT '(INC-GREEN)',
+    `CumulativeDeaths`              int(11) NOT NULL DEFAULT '0',
+    `CumulativeDischarged`          int(11) NOT NULL DEFAULT '0' COMMENT 'Deaths + Recovered (INC-GREEN)',
+    `DailyQuarantineOrdersIssued`   int(11) NOT NULL DEFAULT '0',
+    `TotalCloseContacts`            int(11) NOT NULL DEFAULT '-1',
+    `Quarantined`                   int(11) NOT NULL DEFAULT '0',
+    `CompletedQuarantine`           int(11) NOT NULL DEFAULT '0' COMMENT '(INC-GREEN)',
+    `QUO_Pending`                   int(11) NOT NULL DEFAULT '0',
+    `QUO_TransferHospital`          int(11) NOT NULL DEFAULT '0',
+    `QUO_NonGazettedDorm`           int(11) NOT NULL DEFAULT '0',
+    `QUO_GazettedDorm`              int(11) NOT NULL DEFAULT '0',
+    `QUO_GovtQuarantinedFacilities` int(11) NOT NULL DEFAULT '0',
+    `QUO_HomeQuarantinedOrder`      int(11) NOT NULL DEFAULT '0'
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
 --
 -- Indexes for dumped tables
@@ -67,8 +70,8 @@ CREATE TABLE `sg_manual_delta` (
 -- Indexes for table `sg_manual_delta`
 --
 ALTER TABLE `sg_manual_delta`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `Day` (`Day`);
+    ADD PRIMARY KEY (`id`),
+    ADD KEY `Day` (`Day`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -78,7 +81,7 @@ ALTER TABLE `sg_manual_delta`
 -- AUTO_INCREMENT for table `sg_manual_delta`
 --
 ALTER TABLE `sg_manual_delta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
@@ -87,8 +90,8 @@ ALTER TABLE `sg_manual_delta`
 -- Constraints for table `sg_manual_delta`
 --
 ALTER TABLE `sg_manual_delta`
-  ADD CONSTRAINT `sg_manual_delta_ibfk_1` FOREIGN KEY (`Day`) REFERENCES `sg_manual_info` (`Day`);
+    ADD CONSTRAINT `sg_manual_delta_ibfk_1` FOREIGN KEY (`Day`) REFERENCES `sg_manual_info` (`Day`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
