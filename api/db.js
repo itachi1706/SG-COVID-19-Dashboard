f79f791aa9f0d5303bf95ba1db598785425c20f0
Kenneth Soh <kennethsohyq@gmail.com>
2022-02-12 16:05:29 +0000

    Changes made to prevent untrusted input to DB queries
    Also updated to mysql2 package instead

--- api/db.js
+++ api/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const {dbConfig} = require('../config');
 const util = require('util');
 
@@ -22,10 +22,5 @@ pool.getConnection((err, connection) => {
 });
 
 pool.query = util.promisify(pool.query);
-
-// con.connect((err) => {
-//     if (err) throw err;
-//     console.log("Connected to MySQL DB");
-// });
 
 module.exports = pool;
