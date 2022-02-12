491203f5447308bcf9f798966cacd87dc96641f4
Kenneth Soh <kennethsohyq@gmail.com>
2022-02-12 18:51:37 +0000

    Use non deprecated functions instead

--- sass-middleware.js
+++ sass-middleware.js
@@ -8,20 +8,19 @@ module.exports = async function(req, res, next) {
   // ignore non-CSS requests
   if (!req.path.endsWith('.css')) return next();
 
-  // derive SCSS filepath from CSS request path
+  // derive SASS filepath from CSS request path
   const file = path.join(process.cwd(), 'public', 'stylesheets', req.path).replace(/css/g, 'sass'); // search in sass folder
   if (!fs.existsSync(file)) return res.status(404).end();
 
   // cache rendered CSS in memory
   let rp = req.path;
   if (!cache[rp]) {
-    cache[rp] = sass.renderSync({
-      file,
-      includePaths: [path.join(process.cwd(), 'node_modules')],
-      outputStyle: (process.env.NODE_ENV === 'production') ? 'compressed' : 'expanded'
+    cache[rp] = sass.compile(file, {
+        includePaths: [path.join(process.cwd(), 'node_modules')],
+        outputStyle: (process.env.NODE_ENV === 'production') ? 'compressed' : 'expanded'
     });
 
-    // watch for changes in .scss
+    // watch for changes in .sass
     fs.watchFile(file, _ => {
         delete cache[rp];
         fs.unwatchFile(file);
